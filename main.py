8d2e1a23bedfa003ca5216aa4d600375febc44ca
Yurii Nikonov <shnknsntourist@gmail.com>
2024-03-26 15:59:21 +0300

    added some backend data

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from auth.router import router as auth_router
 
+
+
 app = FastAPI()
 templates = Jinja2Templates(directory='../templates')
 app.mount(path='/static', app=StaticFiles(directory='../static'), name='static')
 app.include_router(auth_router)
 
-# print(templates.env.list_templates())
 
 
 @app.get('/')
